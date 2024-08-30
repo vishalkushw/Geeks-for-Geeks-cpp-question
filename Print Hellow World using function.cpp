92f34e20ae054a8dcd409a217db0258519644f96
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-30 19:25:06 +0530

    Print Hellow World using function.

---
+++ Print Hellow World using function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Solution{
+    public:
+    void helloWorld()
+    {
+        cout<<"Hello World"; 
+    }
+};
+
+int main()
+{
+    Solution obj;
+    obj.helloWorld();
+}
